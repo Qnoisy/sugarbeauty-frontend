1e2cc4485e163657b05f397174aaf54bea752be4
Aveskulov <WenJoymk3@gmail.com>
2025-05-15 11:36:56 +0200

    refactor(styles): restructure SCSS mixins and fix Sass deprecation warnings

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Bounce, ToastContainer } from 'react-toastify';
+import App from './App.tsx';
+import './common/general.scss';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<BrowserRouter>
+			<ToastContainer
+				position='bottom-right'
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick={false}
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme='light'
+				transition={Bounce}
+			/>
+			<App />
+		</BrowserRouter>
+	</StrictMode>
+);
