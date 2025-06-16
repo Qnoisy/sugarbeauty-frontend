cd1a0d46069aa6d39f1b77a6f2d1e391229e0a25
Aveskulov <WenJoymk3@gmail.com>
2025-06-16 20:19:27 +0200

    feat: styles from cancel pay and success pay components

--- src/pages/CancelPay/CancelPay.tsx
+++ src/pages/CancelPay/CancelPay.tsx
@@ -1,11 +1,15 @@
 import type React from 'react';
+import FormContainer from '../../components/FormContainer/FormContainer';
+import styles from './CancelPay.module.scss';
 
 const CancelPay: React.FC = () => {
 	return (
-		<div>
-			<h1>Payment cancelled</h1>
-			<p>You can try again.</p>
-		</div>
+		<FormContainer title={'Cancel Pay'}>
+			<div className={styles.cancel}>
+				<h1>Payment cancelled</h1>
+				<p>You can try again.</p>
+			</div>
+		</FormContainer>
 	);
 };
 export default CancelPay;
--- src/pages/SuccessPay/SuccessPay.tsx
+++ src/pages/SuccessPay/SuccessPay.tsx
@@ -1,11 +1,15 @@
 import type React from 'react';
+import FormContainer from '../../components/FormContainer/FormContainer';
+import styles from './SuccessPay.module.scss';
 
 const SuccessPay: React.FC = () => {
 	return (
-		<div>
-			<h1>Payment successful!</h1>
-			<p>You can now access the course</p>
-		</div>
+		<FormContainer title='Success'>
+			<div className={styles.success}>
+				<h1>Payment successful!</h1>
+				<p>You can now access the course</p>
+			</div>
+		</FormContainer>
 	);
 };
 export default SuccessPay;
